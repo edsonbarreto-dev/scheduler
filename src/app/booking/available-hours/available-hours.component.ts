931779b9508f3909a6fac9714dbac75c80c33a37
Edson Silva Barreto <edson.barreto@outlook.com>
2026-07-18 18:41:38 -0300

    feat: implement available hours component structure

---
+++ src/app/booking/available-hours/available-hours.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'sh-available-hours',
+  templateUrl: './available-hours.component.html',
+  styleUrls: ['./available-hours.component.css'],
+  standalone: true,
+  imports: [],
+})
+export class AvailableHoursComponent {}
