4e9a71a40290f0ae08ad0e6fedf00324e103eeff
Edson Silva Barreto <edson.barreto@outlook.com>
2026-07-18 18:38:26 -0300

    feat: implement customer component structure

---
+++ src/app/customer/customer.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'sh-customer',
+  templateUrl: './customer.component.html',
+  styleUrls: ['./customer.component.css'],
+  standalone: true,
+  imports: [],
+})
+export class CustomerComponent {}
