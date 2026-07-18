3c2cf7a31448b8571dbbcd319c3cbb2888fdd560
Edson Silva Barreto <edson.barreto@outlook.com>
2026-07-18 18:42:21 -0300

    feat: implement available professionals component structure

---
+++ src/app/booking/available-professionals/available-professionals.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'sh-available-professionals',
+  templateUrl: './available-professionals.component.html',
+  styleUrls: ['./available-professionals.component.css'],
+  standalone: true,
+  imports: [],
+})
+export class AvailableProfessionalsComponent {}
