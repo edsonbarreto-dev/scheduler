5f579dc27e01e0aeaf530101701d707c5b0c1510
Edson Silva Barreto <edson.barreto@outlook.com>
2026-07-18 18:41:05 -0300

    feat: implement available services component structure

---
+++ src/app/booking/available-services/available-services.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'sh-available-services',
+  templateUrl: './available-services.component.html',
+  styleUrls: ['./available-services.component.css'],
+  standalone: true,
+  imports: [],
+})
+export class AvailableServicesComponent {}
