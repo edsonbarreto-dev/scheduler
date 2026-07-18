47cf2caf9ed7882567e24ab3f5172203be870b55
Edson Silva Barreto <edson.barreto@outlook.com>
2026-07-18 18:38:01 -0300

    feat: implement provider component structure

---
+++ src/app/provider/provider.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'sh-provider',
+  templateUrl: './provider.component.html',
+  styleUrls: ['./provider.component.css'],
+  standalone: true,
+  imports: [],
+})
+export class ProviderComponent {}
