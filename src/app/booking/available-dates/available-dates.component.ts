58d11c8ebba1e78600ef48e2eee57d7052a54ff4
Edson Silva Barreto <edson.barreto@outlook.com>
2026-07-18 18:41:57 -0300

    feat: implement available dates component structure

---
+++ src/app/booking/available-dates/available-dates.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'sh-available-dates',
+  templateUrl: './available-dates.component.html',
+  styleUrls: ['./available-dates.component.css'],
+  standalone: true,
+  imports: [],
+})
+export class AvailableDatesComponent {}
